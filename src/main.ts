6cc3dc464db32fb194043ab2233a85ee45ab9ae9
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2023-08-12 09:23:07 +0900

    feat: Add process to main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
-function run() {}
+import { getInputParameter } from "./lib/action";
+import {
+  getTemplateFile,
+  outputMarkdown,
+  readMdFiles,
+  replaceCommentToMd,
+} from "./lib/file";
+
+async function run() {
+  // Get input parameter.
+  const { templatePath, srcDir, destFile } = getInputParameter();
+
+  // Get template file.
+  const template = await getTemplateFile(templatePath);
+
+  // Get insert files.
+  const insertFiles = await readMdFiles(srcDir);
+
+  // Replaces comment placeholders in a template.
+  const replacedData = replaceCommentToMd(template, insertFiles);
+
+  // Output markdown file.
+  outputMarkdown(replacedData, destFile);
+}
 
 void run();
